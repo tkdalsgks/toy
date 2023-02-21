4f635462ad1a060f11a13aff9c7c4cc8cbb35e19
DESKTOP-OA3MVGP\tkdalsgks <alstkdgks@gmail.com>
2023-02-21 11:45:55 +0900

    feat: 게시글 사진 업로드 기능 추가
    - ckeditor를 활용하여 AWS S3에 사진을 업로드하고 불러오는 기능

--- src/main/java/com/project/toy/file/controller/FileController.java
+++ src/main/java/com/project/toy/file/controller/FileController.java
@@ -1,10 +1,12 @@
 package com.project.toy.file.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.project.toy.file.service.FileService;
 
@@ -15,7 +17,7 @@ public class FileController {
 	private FileService fileService;
 	
 	@PostMapping("/upload")
-	public String fileUploadFromCKEditor(@RequestPart MultipartFile upload) throws Exception {
-		return fileService.upload(upload);
+	public String upload(HttpServletRequest request, HttpServletResponse response, MultipartHttpServletRequest upload) throws Exception {
+		return fileService.upload(request, response, upload);
 	}
 }
