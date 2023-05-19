3368541ba710c9f56ddd3e157c0f7ae46bd63c13
DESKTOP-OA3MVGP\tkdalsgks <alstkdgks@gmail.com>
2023-05-19 17:31:46 +0900

    style: 관리자 페이지 디자인 변경

--- src/main/java/com/project/toy/admin/controller/AdminController.java
+++ src/main/java/com/project/toy/admin/controller/AdminController.java
@@ -2,17 +2,26 @@ package com.project.toy.admin.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.toy.admin.dto.AdminDTO;
 import com.project.toy.admin.service.AdminService;
+import com.project.toy.board.dto.BoardResponseDTO;
+import com.project.toy.common.dto.SearchDTO;
+import com.project.toy.paging.PagingResponse;
 import com.project.toy.user.dto.SessionUser;
 import com.project.toy.user.dto.UserDTO;
 import com.project.toy.user.service.UserService;
@@ -30,21 +39,36 @@ public class AdminController {
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	@GetMapping("/admin")
-	public String admin(Authentication auth, Model model) {
+	public String admin(@ModelAttribute("params") final SearchDTO params, Authentication auth, Model model) {
 		log.info("***** Admin Page Call *****");
 		
 		SessionUser sessionUser = (SessionUser) session.getAttribute("user");
 		UserDTO user = userService.findByUserId(sessionUser.getUserEmail());
 		
 		if(auth != null) {
-			List<UserDTO> selectListUser = adminService.selectListUser();
-			List<AdminDTO> selectAuthModel = adminService.selectAuthModel();
+			PagingResponse<UserDTO> userList = adminService.selectListUser(params);
+			List<AdminDTO> authList = adminService.selectAuthModel();
 
 			model.addAttribute("user", user.getUserNickname());
-			model.addAttribute("selectListUser", selectListUser);
-			model.addAttribute("selectAuthModel", selectAuthModel);
+			model.addAttribute("userList", userList);
+			model.addAttribute("authList", authList);
 			
 		}
 		return "admin/admin";
+	}
+	
+	@Transactional
+	@GetMapping("/admin/detail")
+	public String detail(@RequestParam Long id, AdminDTO params, 
+			HttpServletRequest request, HttpServletResponse response, Authentication auth, Model model) {
+		log.info("# Admin User Page Detail?id = " + id);
+		
+		AdminDTO admin = adminService.findByUserId(id);
+		List<BoardResponseDTO> board = adminService.findByBoardId(id);
+		
+		model.addAttribute("admin", admin);
+		model.addAttribute("board", board);
+		
+		return "admin/detail";
 	}
 }
--- src/main/java/com/project/toy/admin/service/AdminService.java
+++ src/main/java/com/project/toy/admin/service/AdminService.java
@@ -1,11 +1,16 @@
 package com.project.toy.admin.service;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.project.toy.admin.dto.AdminDTO;
 import com.project.toy.admin.mapper.AdminMapper;
+import com.project.toy.board.dto.BoardResponseDTO;
+import com.project.toy.common.dto.SearchDTO;
+import com.project.toy.paging.Pagination;
+import com.project.toy.paging.PagingResponse;
 import com.project.toy.user.dto.UserDTO;
 
 import lombok.RequiredArgsConstructor;
@@ -16,10 +21,28 @@ public class AdminService {
 	
 	private final AdminMapper adminMapper;
 	
-	public List<UserDTO> selectListUser() {
-		return adminMapper.selectListUser();
+	public PagingResponse<UserDTO> selectListUser(final SearchDTO params) {
+		int count = adminMapper.countUser(params);
+		if(count < 1) {
+			return new PagingResponse<>(Collections.emptyList(), null);
+		}
+		
+		Pagination pagination = new Pagination(count, params);
+		params.setPagination(pagination);
+		
+		List<UserDTO> list = adminMapper.selectListUser();
+		
+		return new PagingResponse<>(list, pagination);
 	}
 
+	public AdminDTO findByUserId(Long id) {
+		return adminMapper.findByUserId(id);
+	}
+	
+	public List<BoardResponseDTO> findByBoardId(Long id) {
+		return adminMapper.findByBoardId(id);
+	}
+	
 	public List<AdminDTO> selectAuthModel() {
 		return adminMapper.selectAuthModel();
 	}
