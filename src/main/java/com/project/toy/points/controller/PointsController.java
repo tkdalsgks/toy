87119c129c23a0b6101eabac66529ec8f383ec23
tkdalsgks <alstkdgks@gmail.com>
2023-08-08 16:45:37 +0900

    fix: 하루 단위 첫 로그인 포인트 쌓는 방식 변경
    - 랜덤박스(100~1000 POINT) or 100 POINT

--- src/main/java/com/project/toy/points/controller/PointsController.java
+++ src/main/java/com/project/toy/points/controller/PointsController.java
@@ -3,12 +3,15 @@ package com.project.toy.points.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.JsonObject;
+import com.project.toy.common.dto.MessageDTO;
 import com.project.toy.points.dto.PointsRequestDTO;
 import com.project.toy.points.service.PointsService;
 
@@ -31,6 +34,26 @@ public class PointsController {
 		
 		try {
 			boolean result = pointsService.selectPoints(params);
+			if(result == true) {
+				jsonObj.addProperty("result", result);
+			}
+		} catch(DataAccessException e) {
+			jsonObj.addProperty("message", "데이터베이스 처리 과정에 문제가 발생하였습니다.");
+		} catch(Exception e) {
+			jsonObj.addProperty("message", "시스템에 문제가 발생하였습니다.");
+		}
+		
+		return jsonObj;
+	}
+	
+	@PostMapping("/save")
+	public JsonObject save(@RequestBody PointsRequestDTO params) {
+		log.info("##### Points Save __ API #####");
+		
+		JsonObject jsonObj = new JsonObject();
+		
+		try {
+			boolean result = pointsService.savePoints(params);
 			if(result == true) {
 				jsonObj.addProperty("result", result);
 			}
